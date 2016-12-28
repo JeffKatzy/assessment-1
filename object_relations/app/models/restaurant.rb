05bb4b0b25f2cd2eddb7ea5f2bca546f75f7f898
JeffKatzy <JeffreyEricKatz@gmail.com>
2016-12-28 16:06:09 -0500

    Update restaurant.rb

--- object_relations/app/models/restaurant.rb
+++ object_relations/app/models/restaurant.rb
@@ -23,6 +23,7 @@ class Restaurant
   end
 
   def self.find_by_name(name)
+#     don't need a return when its the last line in ruby
     return Restaurant.all.select {|restaurant| restaurant.name == name}
   end
 end
